ab03357786d56e50273bfe8646611d8feacb821f
Pzev <pzev77@gmail.com>
2015-03-20 21:48:53 -0400

    commented Tickers
    temporarily commented unused Tickers for efficiency's sake

--- src/main/java/com/cricketcraft/core/core/GameTick.java
+++ src/main/java/com/cricketcraft/core/core/GameTick.java
@@ -12,13 +12,13 @@ public class GameTick
 
     protected static long milliTick = 0;
 
-    protected static long millisecond50Tick = 0;
+   // protected static long millisecond50Tick = 0;
     protected static long millisecond200Tick = 0;
-    protected static float second1Tick = 0;
-    protected static float second3Tick = 0;
-    protected static float second10Tick = 0;
-    protected static float second30Tick = 0;
-    protected static float minute1Tick = 0;
+    //protected static float second1Tick = 0;
+    //protected static float second3Tick = 0;
+    //protected static float second10Tick = 0;
+    //protected static float second30Tick = 0;
+    //protected static float minute1Tick = 0;
 
     public static void tick()
     {
@@ -26,19 +26,21 @@ public class GameTick
         milliTick = endTime - startTime;
         startTime = System.currentTimeMillis();
 
-        millisecond50Tick += milliTick;
+        //millisecond50Tick += milliTick;
         millisecond200Tick += milliTick;
-        second1Tick += (milliTick / 1000f);
-        second3Tick += (milliTick / 1000f);
-        second10Tick += (milliTick / 1000f);
-        second30Tick += (milliTick / 1000f);
-        minute1Tick += (milliTick / 60000f);
+        //second1Tick += (milliTick / 1000f);
+        //second3Tick += (milliTick / 1000f);
+        //second10Tick += (milliTick / 1000f);
+        //second30Tick += (milliTick / 1000f);
+        //minute1Tick += (milliTick / 60000f);
 
+        /*
         while(millisecond50Tick >= 50)
         {
             millisecond50Tick -= 50;
             Milliseconds50();
         }
+        **/
 
         while(millisecond200Tick >= 200)
         {
@@ -46,6 +48,7 @@ public class GameTick
             Milliseconds200();
         }
 
+        /*
         while(second1Tick >= 1)
         {
             second1Tick -= 1;
@@ -75,6 +78,7 @@ public class GameTick
             minute1Tick -= 1;
             Minutes1();
         }
+        **/
     }
 
 
@@ -90,7 +94,7 @@ public class GameTick
 
     private static void Seconds1()
     {
-        System.out.println("Second");
+
     }
 
     private static void Seconds3()
@@ -110,6 +114,6 @@ public class GameTick
 
     private static void Minutes1()
     {
-        System.out.println("Minute");
+
     }
 }
